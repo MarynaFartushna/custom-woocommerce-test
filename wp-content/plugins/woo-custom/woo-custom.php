6e6378738919dd00c74a468ba93fea8aa90354c4
Maryna dev <maryna.fartushna@gmail.com>
2022-07-12 17:51:24 +0300

    new tab && new input on the product page

--- wp-content/plugins/woo-custom/woo-custom.php
+++ wp-content/plugins/woo-custom/woo-custom.php
@@ -6,11 +6,17 @@
  * Author URI:  https://t.me/Maryna_Far
  * Author:      Maryna Fartushna
  * Version:     1.0.0
+ * Text Domain: woocommerce
+ *
+ * WC requires at least: 6.6
+ * WC tested up to: 6.6.1
  */
 
-
+// Prevent Data Leaks
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 define( 'CW_PLUGIN_URL', __FILE__ );
+
+require_once plugin_dir_path( __FILE__ ) . '/admin/init.php';
